250693b7a810d5cdf9d47875fad4f95c6cb6a22c
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-10 12:05:16 +0530

    table of6

--- Program.cs
+++ Program.cs
@@ -5,3 +5,8 @@ for(int i=1;i<=10;i++)
 {
     Console.WriteLine($"{num}*{i} = \t {(num*i)}");
 }
+int num2 = 6;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{num2}*{i} = \t {(num2 * i)}");
+}
