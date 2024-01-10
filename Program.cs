a4c26d5cdc83c46624e07cb24e351d7208eb8902
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-10 12:20:05 +0530

    7th table2

--- Program.cs
+++ Program.cs
@@ -5,8 +5,15 @@ for(int i=1;i<=10;i++)
 {
     Console.WriteLine($"{num}*{i} = \t {(num*i)}");
 }
+Console.WriteLine("table of 6");
 int num2 = 6;
 for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"{num2}*{i} = \t {(num2 * i)}");
+}
+Console.WriteLine("table of 7");
+int num3 = 7;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{num3}*{i} = \t {(num3 * i)}");
 }
